4a4b07d7b55f7c71ba9d88ff7c358685b97dda7b
Raque10bar <r.diez.barrantes@student.rug.nl>
2017-02-14 23:37:12 +0100

    Phongs model implemented

---
+++ Raytracer/scene.cpp
@@ -0,0 +1,126 @@
+//
+//  Framework for a raytracer
+//  File: scene.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include "scene.h"
+#include "material.h"
+
+Color Scene::trace(const Ray &ray)
+{
+    // Find hit object and distance
+    Hit min_hit(std::numeric_limits<double>::infinity(),Vector());
+    Object *obj = NULL;
+    for (unsigned int i = 0; i < objects.size(); ++i) {
+        Hit hit(objects[i]->intersect(ray));
+        if (hit.t<min_hit.t) {
+            min_hit = hit;
+            obj = objects[i];
+        }
+    }
+
+    // No hit? Return background color.
+    if (!obj) return Color(0.0, 0.0, 0.0);
+
+    Material *material = obj->material;            //the hit objects material
+    Point hit = ray.at(min_hit.t);                 //the hit point
+    Vector N = min_hit.N;                          //the normal at hit point
+    Vector V = -ray.D;                             //the view vector
+
+
+    /****************************************************
+    * This is where you should insert the color
+    * calculation (Phong model).
+    *
+    * Given: material, hit, N, V, lights[]
+    * Sought: color
+    *
+    * Hints: (see triple.h)
+    *        Triple.dot(Vector) dot product
+    *        Vector+Vector      vector sum
+    *        Vector-Vector      vector difference
+    *        Point-Point        yields vector
+    *        Vector.normalize() normalizes vector, returns length
+    *        double*Color        scales each color component (r,g,b)
+    *        Color*Color        dito
+    *        pow(a,b)           a to the power of b
+    ****************************************************/
+
+    Color resultColor; // place holder
+    
+    Color ambient;
+    Color diffuse;
+    Color specular;
+    
+    double NdotL;
+    double RdotV;
+    
+    Vector L;
+    Vector R;
+    
+    for (int i = 0; i < lights.size(); i++) {
+        
+        L = (lights[i]->position - hit).normalized();
+        NdotL = N.dot(L);
+        
+        if (NdotL < 0) {
+            NdotL = 0;
+        }
+        
+        R = 2 * NdotL * N - L;
+        RdotV = R.dot(V);
+        
+        if (RdotV < 0) {
+            RdotV = 0;
+        }
+        
+        diffuse += NdotL * lights[i]->color * material->color * material->kd;
+        specular += pow(RdotV, material->n) * lights[i]->color * material->ks;
+    }
+    
+    ambient = material->color * material->ka;
+    
+    resultColor = ambient + diffuse + specular;
+    return resultColor;
+}
+
+void Scene::render(Image &img)
+{
+    int w = img.width();
+    int h = img.height();
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            Point pixel(x+0.5, h-1-y+0.5, 0);
+            Ray ray(eye, (pixel-eye).normalized());
+            Color col = trace(ray);
+            col.clamp();
+            img(x,y) = col;
+        }
+    }
+}
+
+void Scene::addObject(Object *o)
+{
+    objects.push_back(o);
+}
+
+void Scene::addLight(Light *l)
+{
+    lights.push_back(l);
+}
+
+void Scene::setEye(Triple e)
+{
+    eye = e;
+}
---
+++ Raytracer/sphere.cpp
@@ -0,0 +1,78 @@
+//
+//  Framework for a raytracer
+//  File: sphere.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include "sphere.h"
+#include <iostream>
+#include <math.h>
+
+/************************** Sphere **********************************/
+
+Hit Sphere::intersect(const Ray &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    * 
+    * Insert calculation of ray/sphere intersection here. 
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+
+    // place holder for actual intersection calculation
+
+    double b = 2 * (ray.O - position).dot(ray.D);
+    double c = (ray.O - position).dot(ray.O - position) - r*r;
+    
+    double d = b*b - 4*c;
+    double dist = -1;
+    
+    if (d == 0) {
+        dist = -b/2;
+    } else if (d > 0) {
+        dist = (-b - sqrt(d))/2;
+    }
+    
+    if (dist < 0) {
+        return Hit::NO_HIT();
+    }
+    
+    double t = dist;
+
+    
+    /****************************************************
+    * RT1.2: NORMAL CALCULATION
+    *
+    * Given: t, C, r
+    * Sought: N
+    * 
+    * Insert calculation of the sphere's normal at the intersection point.
+    ****************************************************/
+
+    Point intersection = ray.at(t);
+    Vector dir = intersection - position;
+    
+    //Why r squared? O.O
+    Vector N = (intersection + dir*r*r).normalized();
+
+    return Hit(t,N);
+}
